4d984b0285a76d5c75861372f8d708896e61cf3b
David Romoff <74327064+DavidRomoff@users.noreply.github.com>
2024-10-04 21:19:40 -0400

    Update test_UnitTests.py

--- test_UnitTests.py
+++ test_UnitTests.py
@@ -1,16 +1,20 @@
+import inspect
+
 import WhoAmI_File
 def test_WhoAmI():
     assert WhoAmI_File.WhoAmI() != ''
 
-       
 import BondPrice_File
 def test_getBondPrice():
     assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  1)) == 2170604
     assert round(BondPrice_File.getBondPrice(.03, 2000000, .04, 10,  2)) == 2171686
+    assert 'for' not in inspect.getsource(BondPrice_File.getBondPrice)
 
 import BondDuration_File
 def test_getBondDuration():
-    assert round(BondDuration_File.getBondDuration(.03, 2000000, .04, 10,1),2) == 8.51
+    assert round(BondDuration_File.getBondDuration(.03, 2000000, .04, 10, 1), 2) == 8.51    
+    assert round(BondDuration_File.getBondDuration(.03, 2000000, .04, 10, 2), 2) == 8.42
+    assert 'for' not in inspect.getsource(BondPrice_File.getBondPrice)
 
 import FizzBuzz_File
 def test_FizzBuzz():
@@ -18,3 +22,4 @@ def test_FizzBuzz():
     assert x[0] == "buzz"
     assert (x[1]) == 41
     assert x[5] == "fizzbuzz"
+    assert 'for' not in inspect.getsource(BondPrice_File.getBondPrice)
